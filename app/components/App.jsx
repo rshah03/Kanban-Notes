b4303453d9feb86ed01bff0c0b4a3ce7ddaba79a
shahronak1996@gmail.com <Ronak@MacBook-Air-2.local>
2017-07-06 17:31:02 -0500

    Ported methods

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -23,44 +23,22 @@ class App extends Component {
 	}
 
 	addNote = () => {
-		this.setState({
-			notes: this.state.notes.concat([{
-				id: uuid.v4(),
-				task: 'New Task'
-			}])
+		this.props.NoteActions.create({
+			id: uuid.v4(),
+			task: 'New Task'
 		});
 	}
 
 	deleteNote = (id, e) => {
-		e.stopPropagation();
-		this.setState({
-			notes: this.state.notes.filter(note => note.id !== id)
-		});
+		this.props.NoteActions.delete(id);
 	}
 
 	activateNoteEdit = (id) => {
-		this.setState({
-			notes: this.state.notes.map(note => {
-				if(note.id === id) {
-					note.editing = true;
-				}
-
-				return note;
-			})
-		});
+		this.props.NoteActions.update({id, editing: true});
 	}
 
 	editNote = (id, task) => {
-		this.setState({
-			notes: this.state.notes.map(note => {
-				if(note.id === id) {
-					note.editing = false;
-					note.task = task;
-				}
-
-				return note;
-			})
-		});
+		this.props.NoteActions.update({id, task, editing: false});
 	}
 }
 
--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -19,15 +19,26 @@ export default class NoteStore {
     }
 
     create(note) {
-        console.log('Create note', note);
+        this.setState({
+            notes: this.notes.concat(note)
+        });
     }
 
     update(updatedNote) {
-        console.log('Update note', updatedNote);
+        this.setState({
+            notes: this.notes.map(note => {
+                if(note.id === updatedNote.id) {
+                    return Object.assign({}, note, updatedNote);
+                }
+                return note;
+            })
+        });
     }
 
     delete(id){
-        console.log('Deleted note', id);
+        this.setState({
+            notes: this.notes.filter(note => note.id !== id)
+        });
     }
 
-}
+}   
