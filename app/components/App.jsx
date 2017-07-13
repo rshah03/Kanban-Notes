17d36596bc98723a19724495e923e7ffe27c1b38
Minty <Ronak>
2017-07-13 00:04:55 -0500

    Added lane structure

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,48 +3,28 @@ import Notes from './Notes';
 import uuid from 'uuid';
 import connect from '../libs/connect';
 import NoteActions from '../actions/NoteActions';
+import Lanes from './Lanes';
+import LaneActions from '../actions/LaneActions';
 
-class App extends Component {
-
-	render() {
-		const {notes} = this.props;
-
-		return (
-			<div>
-				<button className='add-note' onClick={this.addNote}>+</button>
-				<Notes
-					notes={notes}
-					onNoteClick={this.activateNoteEdit}
-					onEdit={this.editNote}
-					onDelete={this.deleteNote}
-				/>
-			</div>
-		);
-	}
-
-	addNote = () => {
-		this.props.NoteActions.create({
+const App = ({LaneActions, lanes}) => {
+	const addLane = () => {
+		LaneActions.create({
 			id: uuid.v4(),
-			task: 'New Task'
+			name: 'New lane'
 		});
-	}
+	};
 
-	deleteNote = (id, e) => {
-		this.props.NoteActions.delete(id);
-	}
-
-	activateNoteEdit = (id) => {
-		this.props.NoteActions.update({id, editing: true});
-	}
-
-	editNote = (id, task) => {
-		this.props.NoteActions.update({id, task, editing: false});
-	}
-}
+	return (
+		<div>
+			<button className="add-lane" onClick={addLane}>+</button>
+			<Lanes lanes={lanes} />
+		</div>
+	);
+};
 
 //export default App;
-export default connect(({notes}) => ({
-	notes
+export default connect(({lanes}) => ({
+	lanes
 }), {
-	NoteActions
+	LaneActions
 })(App)
